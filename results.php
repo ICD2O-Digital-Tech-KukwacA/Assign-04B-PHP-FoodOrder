c800b98c4a5be7344bc9894f5502ffbc2cc246a9
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-16 15:03:49 +0000

    Testing17

--- results.php
+++ results.php
@@ -1,78 +1,88 @@
-<?php 
-    // get the order from the form
+<?php
     // set the constants for the prices
-    const LIMITED = 7.75;
-    const GREAT = 9.75;
-    const MONSTER = 14.25;
-    const MEAT_TOPPING = 2.99;
-    const VEGGIE_TOPPING = 1.99;
-    const SAUCE = 0.50;
-    const EXTRAS = 1.50;
-    // set the constants for the tax
-    const TAX = 0.13;
-    // set the constants for the sizes
-    const SMALL_SIZE = "Limited";
-    const MEDIUM_SIZE = "Great";
-    const LARGE_SIZE = "Monster";
-    //set the variables for the order
+    const SIZE_LIMITED = 7.75;
+    const SIZE_GREAT = 9.75;
+    const SIZE_MONSTER = 14.25;
+    CONST HST = 0.13;
+    const MEAT_PRICE = 2.99;
+    const VEGGIES_PRICE = 1.99;
+    const SAUCE_PRICE = 0.50;
+    const EXTRAS_PRICE = 1.50;
+
+    // set the order from the form
     $size = $_POST['size'];
     $meat = $_POST['meatToppings'];
-    $veggie = $_POST['veggieToppings'];
+    $veggies = $_POST['veggiesToppings'];
     $sauce = $_POST['sauce'];
     $extras = $_POST['extras'];
-    $meatToppings = "Bacon || Pulled Pork || Ground Beef || Chicken Breast || Karaage Chicken || Popcorn Chicken || Philly Steak || Corn dog slices || Premium";
-    $veggieToppings = "Caramelized Onions || Mushrooms || Green Pepper || Green Onions || Sun Dried Tomatoes || Jalapenos || Dill Pickles";
-    $sauces = "B.B.Q || Sweet Mustard || Garlic Mayo || Burger Sauce || Sriracha || Buffalo || General TSO || Hot Honey || Ranch || Sweet Sriracha B.B.Q";
-    $extraToppings = "Extra cheese curds || Extra gravy || Gluten free gravy";
-    if ($size == SMALL_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = LIMITED;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+
+    if ($size == "Limited") {
+        $sizePrice = SIZE_LIMITED;
+    } elseif ($size == "Great") {
+        $sizePrice = SIZE_GREAT;
+    } elseif ($size == "Monster") {
+        $sizePrice = SIZE_MONSTER;
+    } else {
+        $sizePrice = 0;
     }
-    $subtotal = $size * SMALL_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    elseif ($size == MEDIUM_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = GREAT;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+    // set the price of the meat
+    if ($meat == "Bacon") {
+        $meatPrice = MEAT_PRICE;
+    } elseif ($meat == "Pulled Pork") {
+        $meatPrice = MEAT_PRICE;
+    } elseif ($meat == "Ground Beef") {
+        $meatPrice = MEAT_PRICE;
+    } elseif ($meat == "Chicken Breast") {
+        $meatPrice = MEAT_PRICE;
+    } else {
+        $meatPrice = 0;
     }
-    $subtotal = $size * MEDIUM_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    elseif ($size == LARGE_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = MONSTER;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+    // set the price of the veggies
+    if ($veggies == "Caramelized onions") {
+        $veggiesPrice = VEGGIES_PRICE;
+    } elseif ($veggies == "Mushrooms") {
+        $veggiesPrice = VEGGIES_PRICE;
+    } elseif ($veggies == "Green Pepper") {
+        $veggiesPrice = VEGGIES_PRICE;
+    } elseif ($veggies == "Green Onions") {
+        $veggiesPrice = VEGGIES_PRICE;
+    } else {
+        $veggiesPrice = 0;
     }
-    $subtotal = $size * MONSTER_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    elseif ($size == SMALL_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = LIMITED;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+    // set the price of the sauce
+    if ($sauce == "B.B.Q") {
+        $saucePrice = SAUCE_PRICE;
+    } elseif ($sauce == "Sweet Mustard") {
+        $saucePrice = SAUCE_PRICE;
+    } elseif ($sauce == "Garlic Mayo") {
+        $saucePrice = SAUCE_PRICE;
+    } elseif ($sauce == "Burger Sauce") {
+        $saucePrice = SAUCE_PRICE;
+    } else {
+        $saucePrice = 0;
     }
-    $subtotal = $size * SMALL_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    elseif ($size == MEDIUM_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = GREAT;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+    // set the price of the extras
+    if ($extras == "Extra Cheese Curds") {
+        $extrasPrice = EXTRAS_PRICE;
+    } elseif ($extras == "Extra Gravy") {
+        $extrasPrice = EXTRAS_PRICE;
+    } elseif ($extras == "Gluten-Free Gravy") {
+        $extrasPrice = EXTRAS_PRICE;
+    } elseif ($extras == "None") {
+        $extrasPrice = 0;
+    } else {
+        $extrasPrice = 0;
     }
-    $subtotal = $size * MEDIUM_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    elseif ($size == LARGE_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = MONSTER;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
-    }
-    $subtotal = $size * MONSTER_SIZE + $meat * MEAT_TOPPING + $veggie * VEGGIE_TOPPING + $sauce * SAUCE + $extras * EXTRAS;
-    $tax = $subtotal * TAX;
-    $total = $subtotal + $tax;
-    else {
-        echo "Please place you order.";
-    }
+    // calculate the total price
+    $subtotal = $sizePrice + $meatPrice + $veggiesPrice + $saucePrice + $extrasPrice;
+    // calculate the HST
+    $hst = $subtotal * HST;
+    // calculate the total price with HST
+    $totalPriceWithHST = $totalPrice + $hst;
+    // format the subtotal
+    $subtotal = number_format($subtotal, 2);
+    // format the HST
+    $hst = number_format($hst, 2);
+    // format the total price with HST
+    $totalPriceWithHST = number_format($totalPriceWithHST, 2);
 ?>
