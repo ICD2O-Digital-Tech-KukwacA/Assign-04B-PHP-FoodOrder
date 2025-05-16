8884fa620527ad8ee94f6a5f028a9fb2600faff9
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-16 14:18:00 +0000

    Testing13

--- results.php
+++ results.php
@@ -1,18 +1,5 @@
 <?php 
     // get the order from the form
-    $size = $_POST['size'];
-    $sizePrice = "";
-    $meat = $_POST['meatToppings'];
-    $meatPrice = "";
-    $veggie = $_POST['veggieToppings'];
-    $veggiePrice = "";
-    $sauce = $_POST['sauce'];
-    $saucePrice = "";
-    $extras = $_POST['extras'];
-    $extrasPrice = "";
-    $subtotal = 0;
-    $tax = 0;
-    $total = 0;
     // set the constants for the prices
     const LIMITED = 7.75;
     const GREAT = 9.75;
@@ -27,37 +14,48 @@
     const SMALL_SIZE = "Limited";
     const MEDIUM_SIZE = "Great";
     const LARGE_SIZE = "Monster";
-    $meatToppings = "Any meat topping";
-    $veggieToppings = "Any veggie topping";
-    $sauces = "Any sauce";
-    $extraToppings = "Any extras";
+    //set the variables for the order
+    $size = $_POST['size'];
+    $meat = $_POST['meatToppings'];
+    $veggie = $_POST['veggieToppings'];
+    $sauce = $_POST['sauce'];
+    $extras = $_POST['extras'];
+    $subtotal = "";
+    $tax = "";
+    $total = "";
+    $meatToppings = "Bacon || Pulled Pork || Ground Beef || Chicken Breast || Karaage Chicken || Popcorn Chicken || Philly Steak || Corn dog slices || Premium";
+    $veggieToppings = "Caramelized Onions || Mushrooms || Green Pepper || Green Onions || Sun Dried Tomatoes || Jalapenos || Dill Pickles";
+    $sauces = "B.B.Q || Sweet Mustard || Garlic Mayo || Burger Sauce || Sriracha || Buffalo || General TSO || Hot Honey || Ranch || Sweet Sriracha B.B.Q";
+    $extraToppings = "Extra cheese curds || Extra gravy || Gluten free gravy";
     if ($size == SMALL_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = LIMITED + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+        echo $subtotal = LIMITED + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
+        echo $tax = $subtotal * TAX;
+        echo $total = $subtotal + $tax;
     }
     elseif ($size == MEDIUM_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = GREAT + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+        echo $subtotal = GREAT + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
+        echo $tax = $subtotal * TAX;
+        echo $total = $subtotal + $tax;
     }
     elseif ($size == LARGE_SIZE && $meat == $meatToppings && $veggie == $veggieToppings && $sauce == $sauces && $extras == $extraToppings) {
-        $subtotal = MONSTER + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
+        echo $subtotal = MONSTER + MEAT_TOPPING + VEGGIE_TOPPING + SAUCE + EXTRAS;
+        echo $tax = $subtotal * TAX;
+        echo $total = $subtotal + $tax;
     }
     elseif ($size == SMALL_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = LIMITED;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+        echo $subtotal = LIMITED;
+        echo $tax = $subtotal * TAX;
+        echo $total = $subtotal + $tax;
     }
     elseif ($size == MEDIUM_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = GREAT;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+        echo $subtotal = GREAT;
+        echo $tax = $subtotal * TAX;
+        echo$total = $subtotal + $tax;
     }
     elseif ($size == LARGE_SIZE && $meat != $meatToppings && $veggie != $veggieToppings && $sauce != $sauces && $extras != $extraToppings) {
-        $subtotal = MONSTER;
-        $tax = $subtotal * TAX;
-        $total = $subtotal + $tax;
+        echo $subtotal = MONSTER;
+        echo $tax = $subtotal * TAX;
+        echo $total = $subtotal + $tax;
     }
     else {
         echo "Please place you order.";
