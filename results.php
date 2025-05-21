a4b3f718fcdae08f319bad1ca2193770aa8dbe70
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-21 11:52:53 +0000

    Testing22

--- results.php
+++ results.php
@@ -3,7 +3,7 @@
     const SIZE_LIMITED = 7.75;
     const SIZE_GREAT = 9.75;
     const SIZE_MONSTER = 14.25;
-    CONST HST = 0.13;
+    const HST = 0.13;
     const MEAT_PRICE = 2.99;
     const VEGGIES_PRICE = 1.99;
     const SAUCE_PRICE = 0.50;
@@ -16,6 +16,20 @@
     $sauce = $_POST['sauce'];
     $extras = $_POST['extras'];
 
+     // calculate the total price
+    $subtotal = $sizePrice + $meatPrice + $veggiesPrice + $saucePrice + $extrasPrice;
+    // calculate the HST
+    $hst = $subtotal * HST;
+    // calculate the total price with HST
+    $totalPriceWithHST = $subtotal + $hst;
+    // format the subtotal
+    $subtotal = number_format($subtotal, 2);
+    // format the HST
+    $hst = number_format($hst, 2);
+    // format the total price with HST
+    $totalPriceWithHST = number_format($totalPriceWithHST, 2);
+    
+    // set the price of the size
     if ($size == "Limited") {
         $sizePrice = SIZE_LIMITED;
     } elseif ($size == "Great") {
@@ -71,17 +85,4 @@
     } else {
         $extrasPrice = 0;
     }
-
-    // calculate the total price
-    $subtotal = $sizePrice + $meatPrice + $veggiesPrice + $saucePrice + $extrasPrice;
-    // calculate the HST
-    $hst = $subtotal * HST;
-    // calculate the total price with HST
-    $totalPriceWithHST = $subtotal + $hst;
-    // format the subtotal
-    $subtotal = number_format($subtotal, 2);
-    // format the HST
-    $hst = number_format($hst, 2);
-    // format the total price with HST
-    $totalPriceWithHST = number_format($totalPriceWithHST, 2);
 ?>
